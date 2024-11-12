adeb2e81472fe83aac483ba2c57d24264d779c10
Joaquin-Elia <joaquin.elia@hotmail.com>
2024-11-12 19:48:13 -0300

    feat: update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,30 @@
-import authConfig from "@/auth.config"
+import authConfig from "./auth.config"
 import NextAuth from "next-auth"
+import { NextResponse } from "next/server"
 
-export const { auth: middleware } = NextAuth(authConfig)
+const { auth: middleware } = NextAuth(authConfig)
+
+const publicRoutes = [
+    "/",
+    "/login",
+    "/register"
+]
+
+export default middleware((req) => {
+    const { nextUrl, auth } = req
+    // returns true or false
+    const isLoggedIn = !!auth?.user
+
+    console.log({ isLoggedIn });
+
+
+    // proteger /dashboard /admin
+    if (!publicRoutes.includes(nextUrl.pathname) && !isLoggedIn) {
+        return NextResponse.redirect(new URL("/login", nextUrl))
+    }
+
+    return NextResponse.next()
+})
 
 export const config = {
     matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
